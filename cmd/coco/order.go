8d904a2bc3f3f9542dc9f78b2f3758b0adaafc80
kim yongbin <kybinz@gmail.com>
2020-12-20 06:22:57 +0000

    cmd/coco: make order order

--- cmd/coco/order.go
+++ cmd/coco/order.go
@@ -3,6 +3,10 @@ package main
 import (
 	"errors"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func order(args []string) error {
@@ -12,5 +16,29 @@ func order(args []string) error {
 	if len(fargs) == 0 {
 		return errors.New("need a json file to order")
 	}
+	orderfile := fargs[0]
+	data, err := os.Open(orderfile)
+	if err != nil {
+		return err
+	}
+	cli := &http.Client{}
+	addr := os.Getenv("COCO_ADDR")
+	if addr == "" {
+		addr = "localhost:8282"
+	}
+	req, err := http.NewRequest("POST", "http://"+addr+"/api/order", data)
+	if err != nil {
+		return err
+	}
+	resp, err := cli.Do(req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
 	return nil
 }
