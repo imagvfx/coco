ffca597f011f00e87f665e258522c3fe9dfb41cc
kim yongbin <kybinz@gmail.com>
2021-01-31 11:12:52 +0000

    cmd/cocofarm: add comment to uniqueQueue related codes

--- cmd/cocofarm/queue.go
+++ cmd/cocofarm/queue.go
@@ -1,22 +1,30 @@
 package main
 
+// uniqueQueue is a queue that has unique items.
+// uniqueQueue is a queue, so the value pushed first will popped first.
+// Same values cannot be exist in this queue.
 type uniqueQueue struct {
 	has   map[interface{}]bool
 	first *queueItem
 	last  *queueItem
 }
 
+// queueItem is a queueItem that wraps a value.
+// It directs the next queueItem, so the queue can traverse.
 type queueItem struct {
 	v    interface{}
 	next *queueItem
 }
 
+// newUniqueQueue creates a new uniqueQueue.
 func newUniqueQueue() *uniqueQueue {
 	return &uniqueQueue{
 		has: make(map[interface{}]bool),
 	}
 }
 
+// Push pushs a value to the queue.
+// If the same value has already exists in the queue, it does nothing.
 func (q *uniqueQueue) Push(v interface{}) {
 	if q.has[v] {
 		return
@@ -31,6 +39,8 @@ func (q *uniqueQueue) Push(v interface{}) {
 	q.last = item
 }
 
+// Pop pops a value from the queue.
+// If there isn't any value in the queue, it returns nil.
 func (q *uniqueQueue) Pop() interface{} {
 	if q.first == nil {
 		return nil
@@ -46,6 +56,9 @@ func (q *uniqueQueue) Pop() interface{} {
 	return v
 }
 
+// Remove finds and removes the given value from the queue.
+// If the queue has the value, it removes the value and returns true.
+// Otherwise, it does nothing and returns false.
 func (q *uniqueQueue) Remove(v interface{}) bool {
 	if !q.has[v] {
 		return false
